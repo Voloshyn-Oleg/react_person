10f64cff79f09c7713e5f3e0082246b94c218eec
Oleg Voloshyn <voloshyn.oleg.oleg@gmail.com>
2024-04-18 11:23:34 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,7 @@
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age !== undefined && (
-      <p className="Person__age">I am {person.age}</p>
-    )}
+    {person.age >= 0 && <p className="Person__age">I am {person.age}</p>}
     <p className="Person__partner">
       {person.isMarried
         ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
